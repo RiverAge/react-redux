46fc2d46d9fd0945feb2b2947a237417631f64ac
李蒙@Mac Pro <riverage@163.com>
2016-05-25 22:47:58 +0800

    spelling json of voting.jsx

--- voting-client/components/Voting.jsx
+++ voting-client/components/Voting.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default React.creatClass({
+export default React.createClass({
     getPair: function () {
-        return this.props.piar || [];
+        return this.props.pair || [];
     },
     
     render: function () {
