7869fb79a333bf6e7f27e52878c690ca3cd6a422
李蒙@Mac Pro <riverage@163.com>
2016-05-25 23:03:55 +0800

    handle click & test

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -4,14 +4,15 @@ export default React.createClass({
     getPair: function () {
         return this.props.pair || [];
     },
-    
+
     render: function () {
         return <div className="voting">
             {this.getPair().map(entry =>
-               <button key={entry} >
-               <h1>{entry}</h1>
-               </button>
-                )}
+                <button key={entry} 
+                    onClick={() => this.props.vote(entry)}>
+                    <h1>{entry}</h1>
+                </button>
+            ) }
         </div>
     }
 })
