6c74f56677149a047e52895910e4ecd1941dc4f8
李蒙@Mac Pro <riverage@163.com>
2016-05-27 22:04:56 +0800

    create store from app

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
+import {createStore} from 'redux'
+import reducer from './reducer'
 import App from './components/App'
 import Voting from './components/Voting'
 import Results from './components/Results'
+
+const store = createStore(reducer)
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28d Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+})
 
 const pair = ['Trainspotting', '28 Days Later']
 
