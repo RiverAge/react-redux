a8b96501d8e990dd939098835938d20bbbadd610
李蒙@Mac Pro <riverage@163.com>
2016-05-27 21:00:16 +0800

    app router

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import App from './components/App'
 import Voting from './components/Voting'
-
+import Results from './components/Results'
 
 const pair = ['Trainspotting', '28 Days Later']
 
+const routes = <Router component={App}>
+    <Route path="/results" component={Results} />
+    <Route path="/" component={Voting} />
+    </Router>
+
 ReactDom.render(
-    <Voting pair={pair} winner="Trainspotting" />,
+    <Router history={hashHistory} >{routes}</Router>,
     document.getElementById('app')
 )
