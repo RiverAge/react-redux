64091a95742696aa87be62b8e6b66c1ea8e8def7
李蒙@Mac Pro <riverage@163.com>
2016-05-29 12:34:14 +0800

    fix infinate loop for emit & recieve action

--- voting-client/src/remote_action_middleware.js
+++ voting-client/src/remote_action_middleware.js
@@ -1,5 +1,6 @@
 export default socket => store => next => action => {
     if (action.meta && action.meta.remote) {
+        console.log('middleware in actin.', action)
         socket.emit('action', action)
     }
     return next(action)
